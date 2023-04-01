fa4f55a254980bdf6a9b57732736d1aa0c16ccda
Adam Simonini <adamsimonini@pop-os.localdomain>
2023-04-01 13:36:45 -0400

    now outputing redrawn image with face detection

--- src/countFacesTest.ts
+++ src/countFacesTest.ts
@@ -1,30 +1,81 @@
 // Imports the Google Cloud client libraries
 
 import * as vision from "@google-cloud/vision";
-// const vision = require("@google-cloud/vision");
+import * as fs from "fs";
+import * as PImage from "pureimage";
 
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// const bucketName = 'Bucket where the file resides, e.g. my-bucket';
-// const fileName = 'Path to file within bucket, e.g. path/to/image.png';
+export {};
 
-// Performs face detection on the gcs file
-// const [result] = await client.faceDetection("./assets/birthday.jpg");
-const [result] = await client.faceDetection("./assets/worried.jpg");
-const faces = result.faceAnnotations;
-console.log("Faces:");
-if (faces) {
-	faces.forEach((face: any, i: number) => {
-		console.log(`  Face #${i + 1}:`);
-		console.log(`    Joy: ${face.joyLikelihood}`);
-		console.log(`    Anger: ${face.angerLikelihood}`);
-		console.log(`    Sorrow: ${face.sorrowLikelihood}`);
-		console.log(`    Surprise: ${face.surpriseLikelihood}`);
-	});
+async function detectFaces(inputFilePath: any) {
+	if (fs.existsSync(inputFilePath)) {
+		// Make a call to the Vision API to detect the faces
+		const request = { image: { source: { filename: inputFilePath } } };
+		const results = await client.faceDetection(request);
+		const faces = results[0].faceAnnotations;
+		if (faces) {
+			const numFaces = faces.length;
+			console.log(`Found ${numFaces} face${numFaces === 1 ? "" : "s"}.`);
+			return faces;
+		}
+		return Error("No face found in image");
+	} else {
+		console.warn(`File at ${inputFilePath} doesn't exist`);
+	}
 }
 
-export {};
+async function highlightFaces(inputFile: string, faces: any, outputFile: string, PImage: any) {
+	// Open the original image
+	const stream = fs.createReadStream(inputFile);
+	let promise;
+	if (inputFile.match(/\.jpg$/)) {
+		promise = PImage.decodeJPEGFromStream(stream);
+	} else if (inputFile.match(/\.png$/)) {
+		promise = PImage.decodePNGFromStream(stream);
+	} else {
+		throw new Error(`Unknown filename extension ${inputFile}`);
+	}
+	const img = await promise;
+	const context = img.getContext("2d");
+	context.drawImage(img, 0, 0, img.width, img.height, 0, 0);
+
+	// Now draw boxes around all the faces
+	context.strokeStyle = "rgba(0,255,0,0.8)";
+	context.lineWidth = "5";
+
+	faces.forEach((face: any) => {
+		context.beginPath();
+		let origX = 0;
+		let origY = 0;
+		face.boundingPoly.vertices.forEach((bounds: any, i: number) => {
+			if (i === 0) {
+				origX = bounds.x;
+				origY = bounds.y;
+				context.moveTo(bounds.x, bounds.y);
+			} else {
+				context.lineTo(bounds.x, bounds.y);
+			}
+		});
+		context.lineTo(origX, origY);
+		context.stroke();
+	});
+
+	// Write the result to a file
+	console.log(`Writing to file ${outputFile}`);
+	const writeStream = fs.createWriteStream(outputFile);
+	await PImage.encodePNGToStream(img, writeStream);
+}
+
+// var imageAsBase64 = fs.readFileSync("./assets/birthday.jpg", "base64");
+
+async function main(inputFile: string, outputFile: string) {
+	outputFile = `${outputFile}-highlighted.png` || "out.png";
+	const faces = await detectFaces(inputFile);
+	console.log("Highlighting...");
+	await highlightFaces(inputFile, faces, outputFile, PImage);
+	console.log("Finished!");
+}
+
+main("./assets/birthday.jpg", "birthday");
